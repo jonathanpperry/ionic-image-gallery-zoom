a2032dc2a4f1045b5f161c20eafd4ef245cc38b8
Jonathan Perry <jpper@umich.edu>
2020-10-07 11:02:55 +0900

    Successfully added some dummy images to a slider, and displaying image with tap zoom in on click of the image

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'image-modal',
+    loadChildren: () => import('./image-modal/image-modal.module').then( m => m.ImageModalPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,29 @@
+import { Component } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { ImageModalPage } from "../image-modal/image-modal.page";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
+})
+export class HomePage {
+  sliderOpts = {
+    zoom: false,
+    slidesPerView: 1.5,
+    spaceBetween: 20,
+    centeredSlides: true,
+  };
+  constructor(private modalCtrl: ModalController) {}
+
+  async openPreview(img) {
+    const modal = await this.modalCtrl.create({
+      component: ImageModalPage,
+      cssClass: "transparent-modal",
+      componentProps: { 
+        img,
+      },
+    });
+    modal.present();
+  }
+}
